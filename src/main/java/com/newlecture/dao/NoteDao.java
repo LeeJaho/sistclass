aa909a57e0c22a74ffd1cefcfb6c4772990f9006
Unknown <wkgh0570@gmail.com>
2018-04-11 16:06:40 +0900

    [4/10 SIST class]
    Hibernate, Dao, etc ..  / XML file -> Java config

--- src/main/java/com/newlecture/dao/NoteDao.java
+++ src/main/java/com/newlecture/dao/NoteDao.java
@@ -9,9 +9,10 @@ import com.newlecture.entity.Note;
 
 public interface NoteDao {
 	
-	@Select("select * from Note order by regDate desc" + 
-			"		limit ${(page-1)*15}, 15")
-	List<Note> getList(@Param("page") Integer page);
+	/*마이바티스 어노테이션 방법*/
+	/*@Select("select * from Note order by regDate desc" + 
+			"		limit ${(page-1)*15}, 15")*/
+	List<Note> getList(/*@Param("page") */Integer page);
 	
 	Note get(Integer id);
 	
--- src/main/java/com/newlecture/service/NoteService.java
+++ src/main/java/com/newlecture/service/NoteService.java
@@ -3,10 +3,12 @@ package com.newlecture.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.newlecture.dao.NoteDao;
 import com.newlecture.entity.Note;
 
+@Service
 public class NoteService {
 
 	@Autowired
